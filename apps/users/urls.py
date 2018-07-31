c3f635090e45e77596596f469f52f50878a9e664
yxshyw <515443051@qq.com>
2018-07-31 22:24:37 +0800

    users urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-import xadmin
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('xadmin/', xadmin.site.urls),
-    path('users/', include('users.urls')),
-    # path('courses/', include('courses.urls')),
-    # path('organization/', include('organization.urls')),
-    # path('operation/', include('operation.urls')),
+    path('', views.login, name='login'),
 ]
