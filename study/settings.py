de83b25c0738b9b311c4cca0807fc3125a5e1c7e
yxshyw <515443051@qq.com>
2018-12-18 10:40:42 +0800

    remove admin

--- study/settings.py
+++ study/settings.py
@@ -16,7 +16,7 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
+# sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'courses',
     'organization',
     'operation',
-    'xadmin',
+    # 'xadmin',
     'crispy_forms',
     'captcha',
 ]
--- study/urls.py
+++ study/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-import xadmin
+# import xadmin
 
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
-    path('xadmin/', xadmin.site.urls),
+    # path('xadmin/', xadmin.site.urls),
     path('users/', include('users.urls')),
     path('captcha/', include('captcha.urls')),
     # path('courses/', include('courses.urls')),
